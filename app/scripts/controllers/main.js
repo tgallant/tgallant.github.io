638dc021cdf8bdfd7788ae0748c55be3b896bf8c
Tim Gallant <me@timgallant.us>
2014-09-05 22:05:24 -0700

    cljs-blogger

--- app/scripts/controllers/main.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('tgallantgithubioApp')
-  .controller('MainCtrl', function ($scope) {
-
-  });
--- app/scripts/controllers/post.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('tgallantgithubioApp')
-.controller('PostCtrl', function ($scope, $routeParams) {
-	$scope.getMarkDown = function () {
-		var post = "md/"+$routeParams.post+".md";
-		return post;
-	};
-});
